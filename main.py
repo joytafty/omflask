ea506936f966208174efdc14adb0fbc33468fa8c
joytafty <joytafty@gmail.com>
2013-07-30 23:39:39 -0400

    update main view

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flask, flask.views
 import os
 
 class Main(flask.views.MethodView):
-	def get(self, page="welcome"):
+	def get(self, page="index"):
 		page += ".html"
 		if os.path.isfile('templates/' + page):
 			return flask.render_template(page)
