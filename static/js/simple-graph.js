1a9f4c268b4cc4c7655c9691cf7b2ddae7139bec
joytafty <joytafty@gmail.com>
2013-08-18 23:05:55 -0400

    update simple-graph.js

--- static/js/simple-graph.js
+++ static/js/simple-graph.js
@@ -16,120 +16,118 @@ SimpleGraph = function(elemid, options) {
   this.points = this.options.points;
 
   this.padding = {
-     "top":    this.options.title  ? 40 : 20,
-     "right":  50,
-     "bottom": this.options.xlabel ? 60 : 10,
-     "left":   this.options.ylabel ? 100 : 45
-  };
+   "top":    this.options.title  ? 40 : 20,
+   "right":  50,
+   "bottom": this.options.xlabel ? 60 : 10,
+   "left":   this.options.ylabel ? 100 : 45
+ };
 
-  this.size = {
-    "width":  this.cx - this.padding.left - this.padding.right,
-    "height": this.cy - this.padding.top  - this.padding.bottom
-  };
+ this.size = {
+  "width":  this.cx - this.padding.left - this.padding.right,
+  "height": this.cy - this.padding.top  - this.padding.bottom
+};
 
   // x-scale
   this.x = d3.scale.linear()
-      .domain([this.options.xmin, this.options.xmax])
-      .range([0, this.size.width]);
+  .domain([this.options.xmin, this.options.xmax])
+  .range([0, this.size.width]);
 
   // drag x-axis logic
   this.downx = Math.NaN;
 
   // y-scale (inverted domain)
   this.y = d3.scale.linear()
-      .domain([this.options.ymax, this.options.ymin])
-      .nice()
-      .range([0, this.size.height])
-      .nice();
+  .domain([this.options.ymax, this.options.ymin])
+  .nice()
+  .range([0, this.size.height])
+  .nice();
 
   // drag y-axis logic
-  this.downy = Math.NaN;
+  // this.downy = Math.NaN;
 
   this.dragged = this.selected = null;
 
   this.line = d3.svg.line()
-      .x(function(d, i) { return this.x(this.points[i].x); })
-      .y(function(d, i) { return this.y(this.points[i].y); });
+  .x(function(d, i) { return this.x(this.points[i].x); })
+  .y(function(d, i) { return this.y(this.points[i].y); });
 
   var xrange =  (this.options.xmax - this.options.xmin),
-      yrange2 = (this.options.ymax - this.options.ymin) / 2,
-      yrange4 = yrange2 / 2,
-      datacount = this.size.width/30;
+  yrange2 = (this.options.ymax - this.options.ymin) / 2,
+  yrange4 = yrange2 / 2,
+  datacount = this.size.width/30;
 
-  console.log(this.points)
-  console.log("yrange2 = " + yrange2)
-  console.log("xrange = " + xrange)
-  console.log("datacount = " + datacount)
   // this.points = d3.range(datacount).map(function(i) { 
   //   return { x: i * xrange / datacount, y: this.options.ymin + yrange4 + Math.random() * yrange2 }; 
   // }, self);
-  console.log(this.points)
 
-  this.vis = d3.select(this.chart).append("svg")
-      .attr("width",  this.cx)
-      .attr("height", this.cy)
-      .append("g")
-        .attr("transform", "translate(" + this.padding.left + "," + this.padding.top + ")");
+this.vis = d3.select(this.chart).append("svg")
+.attr("width",  this.cx)
+.attr("height", this.cy)
+.append("g")
+.attr("transform", "translate(" + this.padding.left + "," + this.padding.top + ")");
 
-  this.plot = this.vis.append("rect")
-      .attr("width", this.size.width)
-      .attr("height", this.size.height)
-      .style("fill", "#EEEEEE")
-      .attr("pointer-events", "all")
-      .on("mousedown.drag", self.plot_drag())
-      .on("touchstart.drag", self.plot_drag())
-      this.plot.call(d3.behavior.zoom().x(this.x).y(this.y).on("zoom", this.redraw()));
+this.plot = this.vis.append("rect")
+.attr("width", this.size.width)
+.attr("height", this.size.height)
+.style("fill", "#EEEEEE")
+.attr("pointer-events", "all")
+.on("mousedown.drag", self.plot_drag())
+.on("touchstart.drag", self.plot_drag())
+this.plot.call(d3.behavior.zoom()
+  .x(this.x)
+        // .y(this.y)
+        .on("zoom", this.redraw()));
 
-  this.vis.append("svg")
-      .attr("top", 0)
-      .attr("left", 0)
-      .attr("width", this.size.width)
-      .attr("height", this.size.height)
-      .attr("viewBox", "0 0 "+this.size.width+" "+this.size.height)
-      .attr("class", "line")
-      .append("path")
-          .attr("class", "line")
-          .attr("d", this.line(this.points));
+this.vis.append("svg")
+.attr("top", 0)
+.attr("left", 0)
+.attr("width", this.size.width)
+.attr("height", this.size.height)
+.attr("viewBox", "0 0 "+this.size.width+" "+this.size.height)
+.attr("class", "line")
+.append("path")
+.attr("class", "line")
+.attr("d", this.line(this.points));
 
   // add Chart Title
   if (this.options.title) {
     this.vis.append("text")
-        .attr("class", "axis")
-        .text(this.options.title)
-        .attr("x", this.size.width/2)
-        .attr("dy","-0.8em")
-        .style("text-anchor","middle");
+    .attr("class", "axis")
+    .text(this.options.title)
+    .attr("x", this.size.width/2)
+    .attr("dy","-0.8em")
+    .style("text-anchor","middle");
   }
 
   // Add the x-axis label
   if (this.options.xlabel) {
     this.vis.append("text")
-        .attr("class", "axis")
-        .text(this.options.xlabel)
-        .attr("x", this.size.width/2)
-        .attr("y", this.size.height)
-        .attr("dy","2.4em")
-        .style("text-anchor","middle");
+    .attr("class", "axis")
+    .text(this.options.xlabel)
+    .attr("x", this.size.width/2)
+    .attr("y", this.size.height)
+    .attr("dy","2.4em")
+    .style("text-anchor","middle");
   }
 
   // add y-axis label
   if (this.options.ylabel) {
     this.vis.append("g").append("text")
-        .attr("class", "axis")
-        .text(this.options.ylabel)
-        .style("text-anchor","middle")
-        .attr("transform","translate(" + -70 + " " + this.size.height/2+") rotate(-90)");
+    .attr("class", "axis")
+    .text(this.options.ylabel)
+    .style("text-anchor","middle")
+    .attr("transform","translate(" + -70 + " " + this.size.height/2+") rotate(-90)");
   }
 
   d3.select(this.chart)
-      .on("mousemove.drag", self.mousemove())
-      .on("touchmove.drag", self.mousemove())
-      .on("mouseup.drag",   self.mouseup())
-      .on("touchend.drag",  self.mouseup());
+  .on("mousemove.drag", self.mousemove())
+  .on("touchmove.drag", self.mousemove())
+  .on("mouseup.drag",   self.mouseup())
+  .on("touchend.drag",  self.mouseup());
 
   this.redraw()();
 };
-  
+
 //
 // SimpleGraph methods
 //
@@ -161,103 +159,110 @@ SimpleGraph.prototype.plot_drag = function() {
 SimpleGraph.prototype.update = function() {
   var self = this;
   var lines = this.vis.select("path").attr("d", this.line(this.points));
-        
+
   var circle = this.vis.select("svg").selectAll("circle")
-      .data(this.points, function(d) { return d; });
+  .data(this.points, function(d) { return d; });
 
   circle.enter().append("circle")
-      .attr("class", function(d) { return d === self.selected ? "selected" : null; })
-      .attr("cx",    function(d) { return self.x(d.x); })
-      .attr("cy",    function(d) { return self.y(d.y); })
-      .attr("r", 5.0)
-      .style("cursor", "ns-resize")
-      .on("mousedown.drag",  self.datapoint_drag())
-      .on("touchstart.drag", self.datapoint_drag());
+  .attr("class", function(d) { return d === self.selected ? "selected" : null; })
+  .attr("cx",    function(d) { return self.x(d.x); })
+  .attr("cy",    function(d) { return self.y(d.y); })
+  .attr("r", 5.0)
+  .style("cursor", "ns-resize");
+      // .on("mousedown.drag",  self.datapoint_drag())
+      // .on("touchstart.drag", self.datapoint_drag());
+      // Disabling datapoint moves
 
-  circle
+      circle
       .attr("class", function(d) { return d === self.selected ? "selected" : null; })
       .attr("cx",    function(d) { 
         return self.x(d.x); })
       .attr("cy",    function(d) { return self.y(d.y); });
 
-  circle.exit().remove();
+      circle.exit().remove();
 
-  if (d3.event && d3.event.keyCode) {
-    d3.event.preventDefault();
-    d3.event.stopPropagation();
-  }
-}
+      if (d3.event && d3.event.keyCode) {
+        d3.event.preventDefault();
+        d3.event.stopPropagation();
+      }
+    }
 
-SimpleGraph.prototype.datapoint_drag = function() {
-  var self = this;
-  return function(d) {
-    registerKeyboardHandler(self.keydown());
-    document.onselectstart = function() { return false; };
-    self.selected = self.dragged = d;
-    self.update();
-    
-  }
-};
+    SimpleGraph.prototype.datapoint_drag = function() {
+      var self = this;
+      return function(d) {
+        registerKeyboardHandler(self.keydown());
+        document.onselectstart = function() { return false; };
+        self.selected = self.dragged = d;
+        self.update();
 
-SimpleGraph.prototype.mousemove = function() {
-  var self = this;
-  return function() {
-    var p = d3.svg.mouse(self.vis[0][0]),
-        t = d3.event.changedTouches;
-    
-    if (self.dragged) {
-      self.dragged.y = self.y.invert(Math.max(0, Math.min(self.size.height, p[1])));
-      self.update();
+      }
     };
-    if (!isNaN(self.downx)) {
-      d3.select('body').style("cursor", "ew-resize");
-      var rupx = self.x.invert(p[0]),
+
+    SimpleGraph.prototype.mousemove = function() {
+      var self = this;
+      console.log(self);
+      return function() {
+        var p = d3.svg.mouse(self.vis[0][0]),
+        t = d3.event.changedTouches;
+
+        if (self.dragged) {
+          self.dragged.y = self.y.invert(Math.max(0, Math.min(self.size.height, p[1])));
+          self.update();
+        };
+
+        if (!isNaN(self.downx)) {
+          d3.select('body').style("cursor", "ew-resize");
+          var rupx = self.x.invert(p[0]),
           xaxis1 = self.x.domain()[0],
           xaxis2 = self.x.domain()[1],
           xextent = xaxis2 - xaxis1;
-      if (rupx != 0) {
-        var changex, new_domain;
-        changex = self.downx / rupx;
-        new_domain = [xaxis1, xaxis1 + (xextent * changex)];
-        self.x.domain(new_domain);
-        self.redraw()();
-      }
-      d3.event.preventDefault();
-      d3.event.stopPropagation();
-    };
-    if (!isNaN(self.downy)) {
-      d3.select('body').style("cursor", "ns-resize");
-      var rupy = self.y.invert(p[1]),
+          if (xaxis1 < 0) {
+            xaxis = 0;
+          }
+          if (rupx != 0) {
+            var changex, new_domain;
+            changex = self.downx / rupx;
+            new_domain = [xaxis1, xaxis1 + (xextent * changex)];
+            self.x.domain(new_domain);
+            self.redraw()();
+          }
+          console.log(xaxis1);
+          d3.event.preventDefault();
+          d3.event.stopPropagation();
+        };
+        if (!isNaN(self.downy)) {
+          d3.select('body').style("cursor", "ns-resize");
+          var rupy = self.y.invert(p[1]),
           yaxis1 = self.y.domain()[1],
           yaxis2 = self.y.domain()[0],
           yextent = yaxis2 - yaxis1;
-      if (rupy != 0) {
-        var changey, new_domain;
-        changey = self.downy / rupy;
-        new_domain = [yaxis1 + (yextent * changey), yaxis1];
-        self.y.domain(new_domain);
-        self.redraw()();
+          if (rupy != 0) {
+            var changey, new_domain;
+            changey = self.downy / rupy;
+            new_domain = [yaxis1 + (yextent * changey), yaxis1];
+            self.y.domain(new_domain);
+            self.redraw()();
+          }
+          d3.event.preventDefault();
+          d3.event.stopPropagation();
+        }
       }
-      d3.event.preventDefault();
-      d3.event.stopPropagation();
-    }
-  }
-};
-
-SimpleGraph.prototype.mouseup = function() {
-  var self = this;
-  return function() {
-    document.onselectstart = function() { return true; };
-    d3.select('body').style("cursor", "auto");
-    d3.select('body').style("cursor", "auto");
-    if (!isNaN(self.downx)) {
-      self.redraw()();
-      self.downx = Math.NaN;
-      d3.event.preventDefault();
-      d3.event.stopPropagation();
     };
-    if (!isNaN(self.downy)) {
-      self.redraw()();
+
+    SimpleGraph.prototype.mouseup = function() {
+      var self = this;
+      return function() {
+        document.onselectstart = function() { return true; };
+        d3.select('body').style("cursor", "auto");
+        d3.select('body').style("cursor", "auto");
+        if (!isNaN(self.downx)) {
+          self.redraw()();
+          self.downx = Math.NaN;
+          d3.event.preventDefault();
+          d3.event.stopPropagation();
+        };
+        if (!isNaN(self.downy)) {
+      // self.redraw()();
       self.downy = Math.NaN;
       d3.event.preventDefault();
       d3.event.stopPropagation();
@@ -302,67 +307,70 @@ SimpleGraph.prototype.redraw = function() {
 
     // Regenerate x-ticks…
     var gx = self.vis.selectAll("g.x")
-        .data(self.x.ticks(10), String)
-        .attr("transform", tx);
+    .data(self.x.ticks(10), String)
+    .attr("transform", tx);
 
     gx.select("text")
-        .text(fx);
+    .text(fx);
 
     var gxe = gx.enter().insert("g", "a")
-        .attr("class", "x")
-        .attr("transform", tx);
+    .attr("class", "x")
+    .attr("transform", tx);
 
     gxe.append("line")
-        .attr("stroke", stroke)
-        .attr("y1", 0)
-        .attr("y2", self.size.height);
+    .attr("stroke", stroke)
+    .attr("y1", 0)
+    .attr("y2", self.size.height);
 
     gxe.append("text")
-        .attr("class", "axis")
-        .attr("y", self.size.height)
-        .attr("dy", "1em")
-        .attr("text-anchor", "middle")
-        .text(fx)
-        .style("cursor", "ew-resize")
-        .on("mouseover", function(d) { d3.select(this).style("font-weight", "bold");})
-        .on("mouseout",  function(d) { d3.select(this).style("font-weight", "normal");})
-        .on("mousedown.drag",  self.xaxis_drag())
-        .on("touchstart.drag", self.xaxis_drag());
+    .attr("class", "axis")
+    .attr("y", self.size.height)
+    .attr("dy", "1em")
+    .attr("text-anchor", "middle")
+    .text(fx)
+    .style("cursor", "ew-resize")
+    .on("mouseover", function(d) { d3.select(this).style("font-weight", "bold");})
+    .on("mouseout",  function(d) { d3.select(this).style("font-weight", "normal");})
+    .on("mousedown.drag",  self.xaxis_drag())
+    .on("touchstart.drag", self.xaxis_drag());
 
     gx.exit().remove();
 
     // Regenerate y-ticks…
     var gy = self.vis.selectAll("g.y")
-        .data(self.y.ticks(10), String)
-        .attr("transform", ty);
+    .data(self.y.ticks(10), String)
+    .attr("transform", ty);
 
     gy.select("text")
-        .text(fy);
+    .text(fy);
 
     var gye = gy.enter().insert("g", "a")
-        .attr("class", "y")
-        .attr("transform", ty)
-        .attr("background-fill", "#FFEEB6");
+    .attr("class", "y")
+    .attr("transform", ty)
+    .attr("background-fill", "#FFEEB6");
 
     gye.append("line")
-        .attr("stroke", stroke)
-        .attr("x1", 0)
-        .attr("x2", self.size.width);
+    .attr("stroke", stroke)
+    .attr("x1", 0)
+    .attr("x2", self.size.width);
 
     gye.append("text")
-        .attr("class", "axis")
-        .attr("x", -3)
-        .attr("dy", ".35em")
-        .attr("text-anchor", "end")
-        .text(fy)
-        .style("cursor", "ns-resize")
-        .on("mouseover", function(d) { d3.select(this).style("font-weight", "bold");})
-        .on("mouseout",  function(d) { d3.select(this).style("font-weight", "normal");})
-        .on("mousedown.drag",  self.yaxis_drag())
-        .on("touchstart.drag", self.yaxis_drag());
+    .attr("class", "axis")
+    .attr("x", -3)
+    .attr("dy", ".35em")
+    .attr("text-anchor", "end")
+    .text(fy)
+    .style("cursor", "ns-resize")
+    .on("mouseover", function(d) { d3.select(this).style("font-weight", "bold");})
+    .on("mouseout",  function(d) { d3.select(this).style("font-weight", "normal");})
+    .on("mousedown.drag",  self.yaxis_drag())
+    .on("touchstart.drag", self.yaxis_drag());
 
     gy.exit().remove();
-    self.plot.call(d3.behavior.zoom().x(self.x).y(self.y).on("zoom", self.redraw()));
+    self.plot.call(d3.behavior.zoom()
+      .x(self.x)
+      // .y(self.y)
+      .on("zoom", self.redraw()));
     self.update();    
   }  
 }
